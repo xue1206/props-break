4d8f9db7e846cc3e10676824692f6e52dddfe0ac
xueyong <1085523029@qq.com>
2022-03-12 15:21:17 +0800

    feat: website

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -2,12 +2,12 @@ import type { NextPage } from "next"
 import dynamic from "next/dynamic"
 import { parse } from "@props-break/core"
 import { generate } from "@props-break/generater-react"
+import { useState } from "react"
 
 const Editor = dynamic(() => import("../components/Editor"), { ssr: false })
 
 const defaultValue = `type FormProps = {
-  name: string
-  age: number
+    name: string
 }
 `
 const parseTypeToReactCode = (code: string) => {
@@ -26,17 +26,21 @@ const parseTypeToReactCode = (code: string) => {
 }
 
 const Home: NextPage = () => {
+  const [typeCode, setTypeCode] = useState(defaultValue)
   return (
     <>
-      <h1 className="text-center text-[#A9CEC2] text-4xl pt-6 pb-12 font-bold text-shadow">
+      <h1 className="text-center text-[#A9CEC2] text-4xl pt-6 pb-2 font-bold text-shadow">
         Props Break
       </h1>
+      <h2 className="text-center text-[#A9CEC2] text-normal pb-6 font-normal text-shadow--sm opacity-60">
+        Generate code from type/interface declaration
+      </h2>
       <main className="w-[1300px] flex px-8 justify-between mx-auto">
         <section className="flex-1 max-w-[600px] p-2 border border-[#535E65] rounded">
-          <Editor path="1" defaultValue={defaultValue}></Editor>
+          <Editor path="1" value={typeCode} onChange={(text) => setTypeCode(text || "")}></Editor>
         </section>
         <section className="flex-1 max-w-[600px] p-2 border border-[#535E65] rounded">
-          <Editor path="2" defaultValue={parseTypeToReactCode(defaultValue)}></Editor>
+          <Editor path="2" value={parseTypeToReactCode(typeCode)}></Editor>
         </section>
       </main>
     </>
