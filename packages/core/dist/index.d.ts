5b911bba45d61aab313abd5614e306b36d5a9a35
xueyong <1085523029@qq.com>
2022-03-12 15:34:42 +0800

    feat: update declaration

--- packages/core/dist/index.d.ts
+++ packages/core/dist/index.d.ts
@@ -1,8 +1,6 @@
-declare module "index" {
-    import * as ts from "typescript";
-    export const parse: (code: string) => {
-        name: ts.__String;
-        fields: string[];
-    };
-    export default parse;
-}
+import * as ts from "typescript";
+export declare const parse: (code: string) => {
+    name: ts.__String;
+    fields: string[];
+};
+export default parse;
--- packages/generater-react/dist/index.d.ts
+++ packages/generater-react/dist/index.d.ts
@@ -1,10 +1,8 @@
-declare module "index" {
-    type Options = {
-        componentName: string;
-        propsTypeName: string;
-        propsFields: string[];
-        children?: boolean;
-    };
-    export const generate: (options: Options) => string;
-    export default generate;
-}
+declare type Options = {
+    componentName: string;
+    propsTypeName: string;
+    propsFields: string[];
+    children?: boolean;
+};
+export declare const generate: (options: Options) => string;
+export default generate;
