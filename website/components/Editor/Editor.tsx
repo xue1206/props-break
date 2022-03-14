dc2bc1529fb97a0ca1cca6623ecbf5ff8bf8e86a
xueyong <1085523029@qq.com>
2022-03-14 18:01:58 +0800

    feat: mock React namespace

--- website/components/Editor/Editor.tsx
+++ website/components/Editor/Editor.tsx
@@ -1,6 +1,23 @@
-import MonacoEditor, { EditorProps } from "@monaco-editor/react"
+import MonacoEditor, { BeforeMount, EditorProps, Monaco } from "@monaco-editor/react"
+
+let _installed = false
+const installTypes = (monaco: Monaco) => {
+  if (_installed) return
+  _installed = true
+  // extra libraries
+  var libSource = ["declare namespace React {", "    export type FC<T> = any", "}"].join("\n")
+  var libUri = "ts:filename/react.d.ts"
+  monaco.languages.typescript.javascriptDefaults.addExtraLib(libSource, libUri)
+  // When resolving definitions and references, the editor will try to use created models.
+  // Creating a model for the library allows "peek definition/references" commands to work with the library.
+  monaco.editor.createModel(libSource, "typescript", monaco.Uri.parse(libUri))
+}
 
 const Editor: React.FC<EditorProps> = (props) => {
+  const beforeMount: BeforeMount = (monaco) => {
+    installTypes(monaco)
+  }
+
   return (
     <MonacoEditor
       {...props}
@@ -13,6 +30,7 @@ const Editor: React.FC<EditorProps> = (props) => {
         minimap: { enabled: false },
         ...props.options,
       }}
+      beforeMount={beforeMount}
     ></MonacoEditor>
   )
 }
